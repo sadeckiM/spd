95e4b2b68ec7ecd5275a845af244bda165114781
Wojciech Zielinski <278327@student.pwr.edu.pl>
2026-03-17 11:11:34 +0100

    Define task data type and permutation class

---
+++ lab1/inc/dataTypes.hh
@@ -0,0 +1,22 @@
+#ifndef DATATYPES_HH
+#define DATATYPES_HH
+
+struct Task {
+    int pj;
+    int dj;
+    int rj;
+};
+
+class Permutations {
+    int n;
+    int *perm;
+    public:
+        Permutations();
+        ~Permutations();
+        inline const int & operator [](int i) { return perm[i]; };
+        // add more operators
+        inline void set(int i, int a) { perm[i] = a; };
+        inline int size() { return n; }
+}
+
+#endif // DATATYPES_HH
