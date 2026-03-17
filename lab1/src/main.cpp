5805e91dccd4dc0d2a41779ade8bc8cc9068f000
Cardaver <michal.sadecki@proton.me>
2026-03-17 14:28:12 +0100

    fix: import from file

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -1,21 +1,25 @@
 #include "main.hh"
+#include <iostream>
+#include <sstream>
 
-bool importData(std::string &fileName, std::fstream &dest) {
-    std::ifstream file;
-    file.open(fileName);
+bool importData(std::string &fileName, std::stringstream &dest) {
+    std::ifstream file(fileName);
 
     if(!file.is_open()) {
         std::cerr << "Failed to open file." << std::endl;
         return false;
     }
-    while(file >> dest) {
-        continue;
-    }
+    dest << file.rdbuf();
     file.close();
+    return true;
 }
 
 int main() {
+    std::stringstream dane;
+    std::string file("../test.txt");
     printf("Dupa\n\r");
+    importData(file, dane);
+    std::cout << dane.str() << std::endl;
 
     return 0;
 }
